486dedd223b8261cdb4a8362c5a3473c8e78be55
santiana <santiana@192.168.0.104>
2026-02-02 23:36:20 +0800

    first commit

---
+++ src/main/java/com/hades/HadesApplication.java
@@ -0,0 +1,13 @@
+package com.hades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HadesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hades/HadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
